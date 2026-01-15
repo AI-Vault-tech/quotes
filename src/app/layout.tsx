d99e27ffbf0dadaec049e69d16662a854e4c6f4d
Your Name <you@example.com>
2026-01-15 13:18:17 +0500

    Add Monetag code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -93,6 +93,7 @@ export default function RootLayout({
         <meta name="monetag" content="75721e6fab382e2912f69578a8e0f7df" />
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2894915343289598"
      crossOrigin="anonymous"></script>
+        <script src="https://quge5.com/88/tag.min.js" data-zone="202223" async data-cfasync="false"></script>
       </head>
       <body
         className={`${inter.variable} ${playfairDisplay.variable} ${cinzel.variable} antialiased`}
